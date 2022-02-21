4f0098fb867dfc91e2a8d2c480eb92ec35768775
marcosyoggy <marcos_yoggy@yahoo.com.br>
2022-02-21 19:11:07 -0300

    2102

--- APP_Conversor_de_Moedas/app.js
+++ APP_Conversor_de_Moedas/app.js
@@ -2,14 +2,13 @@ const select_One = document.querySelectorAll('[data-js="currency-one"]')
 const select_Two = document.querySelectorAll('[data-js="currency-two"]')
 const converted_values = document.querySelector('[data-js="converted-value"]')
 const input_Currencies = document.querySelector('[data-js="currency-one-times"]')
+const show_Conversion = document.querySelector('[data-js="conversion-precision"]')
 
 let first_Select = ''
 let second_Select = ''
 let obj_Info = {}
 let ref_Coin_Select01 = ''
 let ref_Coin_Select02 = ''
-
-const show_Unit_Conversion = document.querySelector('[data-js="conversion-precision"]')
 
 const conversions_Values = async coin_Country => {
   const response = await get_Coin_Value(coin_Country)
@@ -30,6 +29,6 @@ input_Currencies.addEventListener('input', () => {
 
     converted_values.textContent = (input_Currencies.value * (second_Select / first_Select)).toFixed(2)
 
-    show_Unit_Conversion.textContent = `1 ${ref_Coin_Select01} = ${second_Select.toFixed(2)} ${ref_Coin_Select02}`
+    show_Conversion.textContent = `1 ${ref_Coin_Select01} = ${second_Select.toFixed(2)} ${ref_Coin_Select02}`
   })
 })
